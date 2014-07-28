8d85e8ddc1a591db1265c6023583a8a1be232fe2
Diana Stancheva <diana@calderra.com>
2014-07-28 20:20:25 +0300

    main

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
 (function () {
 	"use strict";
-	/*require.config({
-		baseUrl: 'tests',
-		paths: {
-			"jquery": "../app/libs/jquery.min",
-			"mustache": "../app/libs/mustache",
-			"studentSystem" : "../app/modules/studentSystem",
-			"mocha": "../../node_modules/mocha/mocha",
-			"chai"   : '../../node_modules/chai/chai'
-		}
-	});*/
-
 	require.config({
 		baseUrl: '.',
 		paths: {
 			"jquery": "app/libs/jquery.min",
 			"mustache": "app/libs/mustache",
-			"studentSystem" : "app/modules/studentSystem",
+			"studentSystem": "app/modules/studentSystem",
 			//"ui": "app/modules/ui",
 			"mocha": "node_modules/mocha/mocha",
-			"chai"   : 'node_modules/chai/chai'
+			"chai": 'node_modules/chai/chai'
 		}
-	 /*shim: {
-			'mocha': {
-				init: function () {
-					this.mocha.setup('bdd');
-					return this.mocha;
-				}
-			},
-			'mustache': {
-				exports: 'Mustache'
-			}
-		}*/
 	});
 
 	require(["jquery", "studentSystem", "app/modules/ui"], function(){
